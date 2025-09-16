feb108774ae31a776160529d77f9082113b23fe1
adnanali9262 <adnan.rafiq173@GMAIL.COM>
2025-09-16 15:10:08 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,14 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('site-tracker-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/app.js',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
-    })
-  );
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open('site-tracker-cache').then(cache=>cache.addAll([
+    '/',
+    '/index.html',
+    '/app.js',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+  ])));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch',event=>{
+  event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));
 });
