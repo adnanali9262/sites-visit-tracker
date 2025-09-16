5606be7b6e063eb58f9f877990db1e32650bf119
adnanali9262 <adnan.rafiq173@GMAIL.COM>
2025-09-16 16:10:17 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open("site-visit-tracker-v1").then((cache) => {
+    caches.open('site-tracker-cache').then((cache) => {
       return cache.addAll([
-        "/sites-visit-tracker/",
-        "/sites-visit-tracker/index.html",
-        "/sites-visit-tracker/app.js",
-        "/sites-visit-tracker/manifest.json",
-        "/sites-visit-tracker/icons/icon-192.png",
-        "/sites-visit-tracker/icons/icon-512.png"
+        '/',
+        '/index.html',
+        '/app.js',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
